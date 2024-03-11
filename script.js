9288fee1e45b30f9e7eee27e2e51c39b67107a2a
TanishaAlilai <152448917+TanishaAlilai@users.noreply.github.com>
2024-03-11 22:16:42 +0000

    These are my new edits.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let age = 25;
 
+const birthYear = 1995;
 
-  document.getElementById("Tanisha").innerHTML = "Tanisha"
+console.log('Age', age);
+
+let sum = age + 5
+console.log ('Your age in 5 years is', sum);
+
+if (age >= 18){
+    console.log('You have access to the adult quizzes on this site');
+}
+
+else {
+  console.log('You are a minor, you do not have access to the adult quizzes on this site');
+  
+}
